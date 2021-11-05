83a7f8920afa3f9480cdfe5418d55ef0ab3e4a27
= <hodaman2012@gmail.com>
2021-11-05 04:59:35 +0200

    links

--- src/pages/project/Providence.js
+++ src/pages/project/Providence.js
@@ -32,36 +32,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom:80,
     marginLeft:-8
   },
-  "@media (max-width: 600px)": {
-    headerContainer:{
-        marginBottom:50,
-  },
-  content:{
-    fontSize: "1rem"
-  },root:{
-    padding:"60px 10px 0",
-  },header:{
-    width:"280px",
-    height:"280px",
-  },
-  link:{
-     fontSize: "1rem",
-     display:"inline"
-  }
-  },
  header:{
     backgroundImage:`url(${header})`,
     width:"400px",
     height:"400px",
     backgroundSize: "cover"
  },
- content:{
-    textAlign:"center",
-    marginBottom:30,
-    marginTop:30,
-    // fontSize:30,
-    fontSize: "0.8rem"
- },
+
  link:{
      textDecoration:"underline",
      fontSize: "0.9rem",
@@ -76,8 +53,33 @@ const useStyles = makeStyles(theme => ({
  },
  textContainer:{
      marginTop:60,
-     marginBottom:200
- }
+     marginBottom:200,
+     padding:"0 225px",
+ },
+ center:{
+   justifyContent:"center",
+   textAlign:"center"
+ },
+ content:{
+  fontSize:"0.9rem"
+},
+ "@media (max-width: 600px)": {
+ textContainer:{
+  padding:0 },
+  headerContainer:{
+      marginBottom:50,
+},
+root:{
+  padding:"60px 10px 0",
+},header:{
+  width:"280px",
+  height:"280px",
+},
+link:{
+   fontSize: "1rem",
+   display:"inline"
+}
+}
 }));
 
 export default function Projec({ }) {
@@ -94,40 +96,59 @@ export default function Projec({ }) {
                 {/* <div className={classes.header} ></div> */}
             </div>
 
-            <Typography className={classes.content} variant="body1">
-              Providence Capsule By  <Link href="https://www.youtube.com/watch?v=LajvidhBK2Y&list=LL&index=4" target="_blank" className={classes.link}>Petrichor</Link> -
+            <Typography className={`${classes.content}  ${classes.center}`}  variant="body1">
+              Providence Capsule By  <Link href="https://www.youtube.com/watch?v=LajvidhBK2Y&list=LL&index=4" target="_blank" className={classes.link}>Petrichor</Link>-
             </Typography>
 
-            <Typography  className={classes.content} variant="body1">
+            <Typography className={`${classes.content}  ${classes.center}`} variant="body1">
                 Protective care, spiritual power, & perpetration for timely eventualities
             </Typography>
 
                  <ImageGalary/>
 
-    <div className = {classes.textContainer} >
+        <div className = {classes.textContainer} >
+            <Typography  className={classes.content}>
+                <b>Production  </b> by  <Link href="https://www.instagram.com/terraform___/?hl=en" target="_blank" className={classes.underlineHover}>Terraform</Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b> Garments </b> by  <Link href="https://www.instagram.com/_petrich0r.us/" target="_blank" className={classes.underlineHover}>Petrichor</Link>
+            </Typography >
 
-        <Typography  className={classes.content}>
-            <b>Production & tee design </b> by  <Link href="https://www.instagram.com/terraform___/?hl=en" target="_blank" className={classes.underlineHover}>Terraform</Link>
-
-        </Typography >
-
-        <Typography  className={classes.content}>
-            <b>Photography</b> by  <Link href="https://www.instagram.com/gabeoviawe/?hl=en" target="_blank" className={classes.underlineHover}> Gabe Oviawe</Link>
-
-        </Typography >
-
-        <Typography  className={classes.content}>
-            <b>Modeled</b> by            <Link href="https://www.instagram.com/jarcor__/?hl=en" target="_blank" className={classes.underlineHover}> Jem Mendoza </Link>
-            
-        </Typography >
-
-        <Typography  className={classes.content}>
-            <b>Casting, styling, & direction</b> by  <Link href="https://www.instagram.com/hugomaxwelll/" target="_blank" className={classes.underlineHover}>Hugo Garibaldi</Link>
-
-        </Typography >
-    </div>
-
+            <Typography  className={classes.content}>
+                <b> Direction, casting, & web design </b> by  <Link href="https://www.instagram.com/hugomaxwelll/" target="_blank" className={classes.underlineHover}>Hugo Garibaldi </Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b> Styled</b> by  <Link href="https://www.instagram.com/m.a.r.i.s.s.a_______/" target="_blank" className={classes.underlineHover}>Marissa Macias</Link> & <Link href="https://www.instagram.com/hugomaxwelll/" target="_blank" className={classes.underlineHover}>Hugo Garibaldi </Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b>Hair & makeup</b> by  <Link href="https://www.instagram.com/bratmobile1999/" target="_blank" className={classes.underlineHover}>Gianna Guerino </Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b>Edits</b> by <Link href="https://www.instagram.com/gabeoviawe/" target="_blank" className={classes.underlineHover}> Gabe Oviawe</Link> &  <Link href="https://www.instagram.com/hugomaxwelll/" target="_blank" className={classes.underlineHover}>Hugo Garibaldi </Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b>Modeled</b> by <Link href="https://www.instagram.com/amandathekidd/" target="_blank" className={classes.underlineHover}> Amanda Fong</Link>
+            </Typography >
+            <Typography  className={classes.content}>
+                <b>Web development</b> by  <Link href="https://www.instagram.com/mahmoud.hammmad/" target="_blank" className={classes.underlineHover}>Mahmoud Hammad </Link>
+            </Typography >
+        </div>
         </Container>
     </div>
   );
 }
+
+/* 
+ - 
+- 
+ - 
+- 
+ - 
+- 
+- 
+
+
+
+
+
+*/
